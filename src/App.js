63b5df5f802150c17328ea694632d0986a95df01
IsaiahGz <isaiahgala99@gmail.com>
2023-05-11 19:11:17 -0400

    Add images and temp header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
+import { useState } from 'react';
+import Header from './components/Header';
+
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className='bg-blue-200' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-					Learn React
-				</a>
-			</header>
+		<div>
+			<Header />
 		</div>
 	);
 }
