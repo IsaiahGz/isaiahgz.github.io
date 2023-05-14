6c8734c81b8614707ba5f3effd4544598231e24d
IsaiahGz <isaiahgala99@gmail.com>
2023-05-14 10:33:13 -0400

    Add profile pic

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import profilePicture from '../images/profile_pic.jpg';
 
 export default function AboutMe() {
 	return (
 		<div className='container mx-auto mt-8 p-4'>
 			<h2 className='text-2xl text-slate-800'>About Me</h2>
 			<div className='mt-4'>
+				<img className='w-40 h-40 rounded-full object-cover' src={profilePicture} alt='Profile' />
 				<p className='text-lg text-slate-800'>
 					My name is Isaiah Galarza and I am a full stack web developer with a passion for learning and problem solving. I am a team player
 					and I am always looking for ways to improve myself and my team. I am currently enrolled in the University of North Carolina at
