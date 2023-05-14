e8c85b21d8ab44439e866e70906f4b5d5acb8f60
IsaiahGz <isaiahgala99@gmail.com>
2023-05-14 01:12:54 -0400

    More mobile friendly navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header({ currentPage, navChoices, setCurrentPage }) {
 		<div className='h-14 sm:h-16 md:h-24'>
 			<div className='bg-cover bg-center p-2 blur-sm h-full' style={{ backgroundImage: `url(${mountains})` }} />
 			<div className='absolute top-0 left-0 w-full p-2'>
-				<h1 style={{ textShadow: '0px 0px 3px white' }} className='text-xl sm:text-3xl md:text-5xl p-2 md:p-4  inline-block text-slate-700'>
+				<h1 style={{ textShadow: '0px 0px 3px white' }} className='text-xl sm:text-3xl md:text-5xl p-2 md:p-4 inline-block text-slate-700'>
 					Isaiah
 				</h1>
 				<Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} navChoices={navChoices} />
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,12 +2,13 @@ export default function Navigation({ currentPage, navChoices, setCurrentPage }) 
 	return (
 		<div className='float-right'>
 			<ul>
-				{navChoices.map((navChoice) => (
+				{navChoices.map((navChoice, idx) => (
 					<li
+						key={idx}
 						onClick={() => {
 							setCurrentPage(navChoice);
 						}}
-						className={`float-left text-base sm:text-lg md:text-2xl p-1 m-1 md:p-2 md:m-2 text-slate-800 ${
+						className={`float-left text-xs sm:text-lg md:text-xl p-1 m-1 md:p-2 md:m-2 text-slate-800 ${
 							currentPage === navChoice ? 'bg-sky-400' : 'bg-sky-200 hover:bg-sky-300'
 						} rounded cursor-pointer`}
 					>
