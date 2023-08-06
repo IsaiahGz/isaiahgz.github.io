6194b03b3370a413c684e63bf5fc02bb803e6a24
IsaiahGz <isaiahgala99@gmail.com>
2023-08-06 15:17:54 -0400

    Add a color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				text: '#000000',
+				background: '#E2E8F0',
+				primary: '#132f53',
+				secondary: '#c1d7f0',
+				accent: '#2b69bb',
+			},
+		},
 	},
 	plugins: [],
 };
