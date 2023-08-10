c264f7fafa9f7a31ed8a5b6d5b23bb5476640b08
IsaiahGz <isaiahgala99@gmail.com>
2023-08-10 13:56:55 -0400

    Replace icons with react-icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import GithubLogo from '../images/github-mark.svg';
-import LinkinLogo from '../images/linkedin.svg';
+import { FaLinkedin, FaGithub } from 'react-icons/fa6';
 
 export default function Footer() {
 	return (
 		<footer className='absolute bottom-0 w-full bg-slate-300 p-4'>
 			<div className='mx-auto w-fit flex space-x-2'>
 				<a href='https://github.com/IsaiahGz'>
-					<img className='h-10' src={GithubLogo} alt='Github logo' />
+					<FaGithub className='text-black text-5xl' />
 				</a>
 				<a href='https://www.linkedin.com/in/isaiah-galarza/'>
-					<img className='h-10' src={LinkinLogo} alt='Linkedin logo' />
+					<FaLinkedin className='text-black text-5xl' />
 				</a>
 			</div>
 		</footer>
