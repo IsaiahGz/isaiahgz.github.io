8ce847ca7386eb65b1fe5b522c4637b2dd4191f2
IsaiahGz <isaiahgala99@gmail.com>
2023-08-10 13:53:48 -0400

    Remove unused code

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,27 +3,7 @@ import profilePicture from '../images/profile_pic.jpg';
 
 export default function AboutMe() {
 	return (
-		<div className='container mx-auto mt-20 p-4'>
-			{/* <h2 className='text-5xl text-slate-800'>About Me</h2>
-			<div className='mt-4'>
-				<img className='w-40 h-40 rounded-full object-cover' src={profilePicture} alt='Profile' />
-				<p className='text-lg text-slate-800'>
-					My name is Isaiah Galarza and I am a full stack web developer with a passion for learning and problem solving. I am a
-					team player and I am always looking for ways to improve myself and my team. I am currently enrolled in the University of
-					North Carolina at Charlotte's Full Stack Web Development Bootcamp. I am always looking for opportunities to learn and
-					grow as a developer.
-				</p>
-				<p className='text-lg text-slate-800 mt-2'>
-					I am currently learning how to create a full stack web application using the MERN stack. The last project I worked on
-					was a full stack web application that allows users to create an account, login, and create blog posts that are stored in
-					a database. The application also allows users to view other users' blog posts and comment on them. This application was
-					created using handlebars, express, sequelize, and mysql. You can view the application here:{' '}
-					<a className='text-cyan-700' href='https://bits-and-bytes.herokuapp.com/'>
-						Bits and Bytes
-					</a>
-					.
-				</p>
-			</div> */}
+		<div className='container mx-auto mt-20'>
 			<h2 className='text-5xl text-slate-800 mb-10'>About Me</h2>
 			<div className='flex flex-wrap'>
 				<div className='w-full lg:w-1/4 mb-10 lg:mb-0 flex flex-col items-center'>
